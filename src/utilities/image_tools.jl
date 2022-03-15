372166e104d9e66a8c81c58f01d862243efccc1d
Naomi Graham <naomigra@cedar1.cedar.computecanada.ca>
2022-03-14 18:55:00 -0700

    finished save_MNIST_images function

--- src/utilities/image_tools.jl
+++ src/utilities/image_tools.jl
@@ -4,17 +4,9 @@ using MLDatasets
 using ImageCore
 
 
-function save_to_images(x_batch, save_dir::String, prefix::String, num_images::Int64)
-    @assert num_images <= size(x_batch)[2]
-    for i=1:num_images
-        save(joinpath(save_dir, "$prefix-$i.png"), colorview(Gray, reshape(x_batch[:, i], 28,28)' ))
-    end
-end
-
-# TO DO : fix this function
 function save_MNIST_images(x_batch, save_dir::String, prefix::String)
     i = 1
-    for x in x_batch # need to figure out how to iterate over the batch
+    for x in eachslice(x_batch,dims = 3)
         convert_and_save_single_MNIST_image(x,save_dir,"$prefix-$i.png")
         i += 1
     end
