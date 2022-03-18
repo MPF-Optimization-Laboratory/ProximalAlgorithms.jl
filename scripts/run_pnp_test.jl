d661b13423731a9d768ad08eb57136d1e40f1108
Naomi Graham <naomigra@cedar5.cedar.computecanada.ca>
2022-03-18 16:12:00 -0700

    fixed an error in run_example, still working on getting interesting output

--- scripts/run_pnp_test.jl
+++ scripts/run_pnp_test.jl
@@ -10,23 +10,28 @@ using ProximalOperators
 
 
 function run_example()
-    T = Float64
+    println(pwd())
+    T = Float32
     R = real(T)
     k = 0.7 #how much we want to reduce (scale) max rank of A
 
     example_index = 10
     model_epoch_number = 20
-    model_dir = "../src/utilities/saved_models/MNIST"
+    model_dir = "./src/utilities/saved_models/MNIST"
     encoder_μ, encoder_logvar, decoder = load_model(model_dir,model_epoch_number)
 
-    function vae_denoiser!(x) 
-        x = reconstruct_images(encoder_μ, encoder_logvar, decoder,x)
+    function vae_denoiser!(x,y)
+        #defining the denoiser this way so that it has the same format as prox!
+        x = reconstruct_images(encoder_μ, encoder_logvar, decoder,y)
     end
 
-    g = vae_denoiser!
+    #g = vae_denoiser!
 
     test_x, test_y = MNIST.testdata(Float32,example_index)
     m,n = size(test_x)
+
+    # look for FFT tutorial for blurring model
+    # sparko test set
     A_full_rank = rand(Float32, (m*n, m*n))
     max_rank = Int64(floor(m*n*k))
     F = svd(A_full_rank)
@@ -42,21 +47,27 @@ function run_example()
     b = A*test_x_vec[:]
 
     f = LeastSquares(A, b)
+
     lam = R(0.1) * norm(A' * b, Inf)
     x0 = zeros(R, m*n)
     y = similar(x0)
     gamma = R(10) / opnorm(A)^2 #constant parameter for douglas rachford
-
+    gamma = 1.0f0
 
     #forward operator
     proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma)
     #backward operator
-    denoiser!(yhat,rhat) = prox!(yhat,g,rhat,gamma)
 
-    pnp_dr_iter = PnpDrsIteration(proxf! = proxf!, denoiser! = denoiser!, uhat0 = x0, gamma = gamma)
+    
+    # TO DO fix denoiser, shouldn't be prox
+    #denoiser!(yhat,rhat) = prox!(yhat,g,rhat,gamma)
 
-    for dr_state in Iterators.take(pnp_dr_iter,20)
-        println(norm(test_x_vec - pnp_dr_state.x)) 
+    #pnp_dr_iter = PnpDrsIteration(proxf! = proxf!, denoiser! = denoiser!, uhat0 = x0, gamma = gamma)
+    pnp_dr_iter = PnpDrsIteration(proxf! = proxf!, denoiser! = vae_denoiser!, uhat0 = x0, gamma = gamma)
+
+
+    for pnp_dr_state in Iterators.take(pnp_dr_iter,100)
+        println(norm(test_x_vec - pnp_dr_state.xhat))
     end
 
 
