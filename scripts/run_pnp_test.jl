17d3f1a84892784de4894c1cc98baa8a6a33ef12
Naomi Graham <naomigra@cedar5.cedar.computecanada.ca>
2022-03-25 18:08:31 -0700

    fixed issue in running example and added two additional examples

--- scripts/run_pnp_test.jl
+++ scripts/run_pnp_test.jl
@@ -9,11 +9,11 @@ using LinearAlgebra
 using ProximalOperators
 
 
-function run_example()
+function run_example_1()
     println(pwd())
     T = Float32
     R = real(T)
-    k = 0.7 #how much we want to reduce (scale) max rank of A
+    k = 0.8 # how much we want to reduce (scale) max rank of A
 
     example_index = 10
     model_epoch_number = 20
@@ -49,10 +49,10 @@ function run_example()
 
     f = LeastSquares(A, b)
 
-    lam = R(0.1) * norm(A' * b, Inf)
+    #lam = R(0.1) * norm(A' * b, Inf)
     #x0 = zeros(R, m*n)
     x0 = test_x_vec[:]
-    y = similar(x0)
+    #y = similar(x0)
     gamma = R(10) / opnorm(A)^2 # constant parameter for douglas rachford
     gamma = 1.0f0
 
@@ -65,8 +65,124 @@ function run_example()
     # TO DO: find a useful way to look at output
     for pnp_dr_state in Iterators.take(pnp_dr_iter,50)
         convert_and_save_single_MNIST_image(pnp_dr_state.xhat,"./scripts/temp/","iteration_$i")
-        #println(norm(test_x_vec - pnp_dr_state.xhat))
+        # println(norm(test_x_vec - pnp_dr_state.xhat))
         println(norm(pnp_dr_state.xhat))
+        i+=1
+    end
+
+
+end
+
+
+
+function run_example_2()
+    T = Float32
+    R = real(T)
+    k = 0.5 # ratio of rows to columns
+
+    example_index = 11
+    model_epoch_number = 20
+    model_dir = "./src/utilities/saved_models/MNIST"
+    encoder_μ, encoder_logvar, decoder = load_model(model_dir,model_epoch_number)
+
+    # backward operator (denoiser)
+    function vae_denoiser!(x,y)
+        # defining the denoiser this way so that it has the same format as prox!
+        x .= reconstruct_images(encoder_μ, encoder_logvar, decoder,y)
+    end
+
+    test_x, test_y = MNIST.testdata(Float32,example_index)
+    test_x_vec = vectorize_and_flip(test_x)
+    convert_and_save_single_MNIST_image(test_x_vec[:],"./scripts/temp/run_example2","ground_truth")
+
+
+    m,n = size(test_x)
+    A = randn(T, Int64(floor(m*n*k)), (m*n)) # wide matrix
+    b = A*test_x_vec[:] + randn(T,Int64(floor(m*n*k)) ) # TO DO: values of b are outside (0,1) range, fix this
+
+    # convert_and_save_single_MNIST_image(clamp.(b,0,1),"./scripts/temp/","observed")
+
+    f = LeastSquares(A, b)
+
+    #lam = R(0.1) * norm(A' * b, Inf)
+    x0 = zeros(R, m*n)
+    #x0 = test_x_vec[:]
+    #y = similar(x0)
+    #gamma = R(10) / opnorm(A)^2 # constant parameter for douglas rachford using g is the l1 norm
+    gamma = 1.0f0
+    println("gamma is", gamma)
+
+    # forward operator
+    proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma)
+    
+    pnp_dr_iter = PnpDrsIteration(proxf! = proxf!, denoiser! = vae_denoiser!, uhat0 = x0, gamma = gamma)
+
+    i = 0
+    # TO DO: find a useful way to look at output
+    for pnp_dr_state in Iterators.take(pnp_dr_iter,50)
+        #convert_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example2/","iteration_$i")
+        resize_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example2/","iteration_$i")
+        println(norm(test_x_vec[:] - pnp_dr_state.xhat))
+        #println(norm(pnp_dr_state.xhat))
+        i+=1
+    end
+
+
+end
+
+
+
+function run_example_3()
+    T = Float32
+    R = real(T)
+    k = 0.5 # ratio of rows to columns
+
+    example_index = 11
+    model_epoch_number = 20
+    model_dir = "./src/utilities/saved_models/MNIST"
+    encoder_μ, encoder_logvar, decoder = load_model(model_dir,model_epoch_number)
+
+    # backward operator (denoiser)
+    #function vae_denoiser!(x,y)
+    function identity_denoiser!(x,y)
+        # defining the denoiser this way so that it has the same format as prox!
+        #x .= reconstruct_images(encoder_μ, encoder_logvar, decoder,y)
+        x.=y
+    end
+
+    test_x, test_y = MNIST.testdata(Float32,example_index)
+    test_x_vec = vectorize_and_flip(test_x)
+    convert_and_save_single_MNIST_image(test_x_vec[:],"./scripts/temp/run_example3","ground_truth")
+
+
+    m,n = size(test_x)
+    A = randn(T, Int64(floor(m*n*k)), (m*n)) # wide matrix
+    b = A*test_x_vec[:] + randn(T,Int64(floor(m*n*k)) ) # TO DO: values of b are outside (0,1) range, fix this
+
+    # convert_and_save_single_MNIST_image(clamp.(b,0,1),"./scripts/temp/","observed")
+
+    f = LeastSquares(A, b)
+
+    #lam = R(0.1) * norm(A' * b, Inf)
+    x0 = zeros(R, m*n)
+    #x0 = test_x_vec[:]
+    #y = similar(x0)
+    #gamma = R(10) / opnorm(A)^2 # constant parameter for douglas rachford using g is the l1 norm
+    gamma = 1.0f0
+    println("gamma is", gamma)
+
+    # forward operator
+    proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma)
+    
+    pnp_dr_iter = PnpDrsIteration(proxf! = proxf!, denoiser! = identity_denoiser!, uhat0 = x0, gamma = gamma)
+
+    i = 0
+    # TO DO: find a useful way to look at output
+    for pnp_dr_state in Iterators.take(pnp_dr_iter,50)
+        #convert_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example2/","iteration_$i")
+        resize_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example3/","iteration_$i")
+        println(norm(test_x_vec[:] - pnp_dr_state.xhat))
+        #println(norm(pnp_dr_state.xhat))
         i+=1
     end
 
