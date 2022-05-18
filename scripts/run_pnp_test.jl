e3f6517ec4cb43c9146fa1d04ceb03e909aec811
Naomi Graham <naomigra@cedar5.cedar.computecanada.ca>
2022-05-18 16:59:10 -0700

    updated examples, example 1 and 4 both working well now

--- scripts/run_pnp_test.jl
+++ scripts/run_pnp_test.jl
@@ -17,7 +17,6 @@ using Zygote
 
 
 function run_example_1()
-    println(pwd())
     T = Float32
     R = real(T)
     k = 0.8 # how much we want to reduce (scale) max rank of A
@@ -44,36 +43,37 @@ function run_example_1()
         F.S[i] = 0
     end
 
-    A = F.U * Diagonal(F.S) * F.Vt
+    A = (F.U * Diagonal(F.S) * F.Vt)/100
     test_x_vec = vectorize_and_flip(test_x)
+    
+    convert_and_save_single_MNIST_image(test_x_vec[:],"./scripts/temp/run_example1","ground_truth")
 
-    convert_and_save_single_MNIST_image(test_x_vec[:],"./scripts/temp/","ground_truth")
-
-
-    b = A*test_x_vec[:] # TO DO: values of b are outside (0,1) range, fix this
-
-    # convert_and_save_single_MNIST_image(clamp.(b,0,1),"./scripts/temp/","observed")
-
+    b = A*test_x_vec[:] # add additive noise as well?
+    
+    b = b + randn(T,Int64(length(b))) #adding noise
     f = LeastSquares(A, b)
 
     #lam = R(0.1) * norm(A' * b, Inf)
-    #x0 = zeros(R, m*n)
-    x0 = test_x_vec[:]
-    #y = similar(x0)
-    gamma = R(10) / opnorm(A)^2 # constant parameter for douglas rachford
-    gamma = 1.0f0
-
+    x0 = zeros(R, m*n)
+    #x0 = test_x_vec[:]
+    #gamma = R(10) / opnorm(A)^2 # constant parameter for douglas rachford
+    #gamma = 1.0f0
+    gamma = R(0.1) #trying different values of gamma
     # forward operator
-    proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma)
+    proxf!(x,z) = prox!(x,f,z,gamma)
     
-    pnp_dr_iter = PnpDrsIteration(proxf! = proxf!, denoiser! = vae_denoiser!, uhat0 = x0, gamma = gamma)
+    pnp_dr_iter = PnpDrsIteration(J_A! = proxf!, J_B! = vae_denoiser!, z0 = x0)
 
     i = 0
     # TO DO: find a useful way to look at output
-    for pnp_dr_state in Iterators.take(pnp_dr_iter,10)
-        #convert_and_save_single_MNIST_image(pnp_dr_state.xhat,"./scripts/temp/","iteration_$i")
-        #println(norm(test_x_vec - pnp_dr_state.xhat))
+    for pnp_dr_state in Iterators.take(pnp_dr_iter,200)
+        println("iteration $i")
         println(norm(pnp_dr_state.res))
+        println("\n")
+        #convert_and_save_single_MNIST_image(pnp_dr_state.x,"./scripts/temp/run_example1/","iteration_$i")
+        
+        resize_and_save_single_MNIST_image(pnp_dr_state.x,"./scripts/temp/run_example1/","iteration_$i")
+        #println(norm(pnp_dr_state.res))
         i+=1
     end
 
@@ -105,7 +105,8 @@ function run_example_2()
 
     m,n = size(test_x)
     A = randn(T, Int64(floor(m*n*k)), (m*n)) # wide matrix
-    b = A*test_x_vec[:] + randn(T,Int64(floor(m*n*k)) ) # TO DO: values of b are outside (0,1) range, fix this
+    # b = Ax + noise
+    b = A*test_x_vec[:] + randn(T,Int64(floor(m*n*k)) ) 
 
     # convert_and_save_single_MNIST_image(clamp.(b,0,1),"./scripts/temp/","observed")
 
@@ -115,8 +116,8 @@ function run_example_2()
     x0 = zeros(R, m*n)
     #x0 = test_x_vec[:]
     #y = similar(x0)
-    #gamma = R(10) / opnorm(A)^2 # constant parameter for douglas rachford using g is the l1 norm
-    gamma = 1.0f0
+    gamma = R(10) / opnorm(A)^2 # constant parameter for douglas rachford using g is the l1 norm
+    #gamma = 1.0f0
     println("gamma is", gamma)
 
     # forward operator
@@ -126,10 +127,10 @@ function run_example_2()
 
     i = 0
     # TO DO: find a useful way to look at output
-    for pnp_dr_state in Iterators.take(pnp_dr_iter,50)
+    for pnp_dr_state in Iterators.take(pnp_dr_iter,10)
         #convert_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example2/","iteration_$i")
         resize_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example2/","iteration_$i")
-        println(norm(state.res))
+        println(norm(pnp_dr_state.res))
         #println(norm(test_x_vec[:] - pnp_dr_state.xhat))
         #println(norm(pnp_dr_state.xhat))
         i+=1
@@ -139,7 +140,7 @@ function run_example_2()
 end
 
 
-
+# in this example we use the identity as a denoiser
 function run_example_3()
     T = Float32
     R = real(T)
@@ -190,7 +191,6 @@ function run_example_3()
         #convert_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example2/","iteration_$i")
         resize_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example3/","iteration_$i")
         println(norm(test_x_vec[:] - pnp_dr_state.xhat))
-        #println(norm(pnp_dr_state.xhat))
         i+=1
     end
 
@@ -223,7 +223,7 @@ function run_denoising_example()
 
 end
 
-
+#examples 4 and 5 are for studying convergence
 function run_example_4()
     T = Float32
     A = T[
@@ -243,30 +243,30 @@ function run_example_4()
     f = LeastSquares(A, b)
     g = NormL1(lam)
 
-    x0 = zeros(R, n) # initial point of zero
+    z0 = zeros(R, n) # initial point of zero
 
     gamma=R(10) / opnorm(A)^2
     gamma = R(1) # gamma doesn't currently do anything in the algorithm!
 
-    proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma) 
-    denoiser!(yhat,rhat) = prox!(yhat,g,rhat,gamma)
+    proxf!(x,z) = prox!(x,f,z,gamma) 
+    denoiser!(y,r) = prox!(y,g,r,gamma)
 
     #dr_iter = DouglasRachfordIteration(f=f, g=g, x0=x0, gamma=gamma)
 
-    #pnp_dr_iter = PnpDrsIteration(proxf! = proxf!, denoiser! = denoiser!, uhat0 = x0, gamma = gamma)
+    pnp_dr_iter = PnpDrsIteration(J_A! = proxf!, J_B! = denoiser!, z0 = z0)
 
-    pnp_dr_iter2 = PnpDrsIteration2(proxf! = proxf!, denoiser! = denoiser!, x0 = x0, gamma = gamma)
+    #pnp_dr_iter2 = PnpDrsIteration2(proxf! = proxf!, denoiser! = denoiser!, x0 = x0, gamma = gamma)
 
     
-    #for pnp_dr_state in Iterators.take(pnp_dr_iter, 15)
-    #    println(norm(pnp_dr_state.res))
-    #end
-
-    for pnp_dr_state in Iterators.take(pnp_dr_iter2, 10)
-        println(norm(pnp_dr_state.dualres)) #should go to zero, stalls around 1.4578023
-        println(norm(pnp_dr_state.x_tilde_prev))
-        #println(norm(pnp_dr_state.μ - pnp_dr_state.ν))
+    for pnp_dr_state in Iterators.take(pnp_dr_iter, 30)
+        println(norm(pnp_dr_state.res))
     end
+
+    #for pnp_dr_state in Iterators.take(pnp_dr_iter2, 10)
+    #    println(norm(pnp_dr_state.dualres)) #should go to zero, stalls around 1.4578023
+    #    println(norm(pnp_dr_state.x_tilde_prev))
+        #println(norm(pnp_dr_state.μ - pnp_dr_state.ν)) # this should go to zero according to BC
+    #end
 end
 
 
@@ -293,12 +293,15 @@ function run_example_5()
     ]
     
     b = T[1.0, 2.0, 3.0, 4.0]
-    #b = T[0.0, 0.0, 0.0, 0.0]
 
     m, n = size(A)
 
     R = real(T)
-    lam = R(0.1) * norm(A' * b, Inf)
+    
+    #lam = R(0.1) * norm(A' * b, Inf)
+    #lam = R(0.5) #what role does lambda play?
+
+    lam = R(1)
 
     f = LeastSquares(A, b)
     g = NormL1(lam)
@@ -308,7 +311,7 @@ function run_example_5()
     gdual = Conjugate(g)
 
     x0 = zeros(R, n)
-
+    u0 = zeros(R, n)
 
     #gamma=R(10) / opnorm(A)^2
 
@@ -316,34 +319,59 @@ function run_example_5()
 
     gamma = R(1)
 
-    #proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma) 
-    proxf!(xhat,uhat) = prox!(xhat,fdual,uhat,gamma) 
+    #proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma) #apply DRS to dual problem 
+    proxf!(out,in) = prox!(out,fdual,in,gamma) 
 
-    #denoiser!(yhat,rhat) = prox!(yhat,g,rhat,gamma)
-    denoiser!(yhat,rhat) = prox!(yhat,gdual,rhat,gamma)
-
-
+    denoiser!(out,in) = prox!(out,g,in,gamma)
+    #denoiser!(out,in) = prox!(out,gdual,in,gamma) #apply DRS to dual problem
 
     #proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma) #in place prox updates xhat
     #denoiser!(yhat,rhat) = prox!(yhat,g,rhat,gamma) #rhat^{k+1} = 2xhat^{k+1} - uhat^{k}
 
     #dr_iter = DouglasRachfordIteration(f=f, g=g, x0=x0, gamma=gamma)
-    admm_iter = AdmmIteration(f = f, g = g, x0 = x0, gamma = gamma)
+    admm_iter = AdmmIteration(f = f, g = g, x0 = u0, gamma = gamma)
     
     pnp_dr_iter2 = PnpDrsIteration2(proxf! = proxf!, denoiser! = denoiser!, x0 = x0, gamma = gamma)
     
-    for admm_state in Iterators.take(admm_iter, 10)
-        println(norm(admm_state.dualres))
-        #println(norm(admm_state.y))
+    i = 0
+
+    for (admm_state, pnp_dr_state) in Iterators.take(zip(admm_iter, pnp_dr_iter2), 1000)
+    #for admm_state in Iterators.take(admm_iter, 5)
+        #append!(dualres_vector,admm_state.dualres)
+        #println(norm(admm_state.u)) # converges to 2.072
+        #println(norm(admm_state.x - admm_state.y))
+        #println("\n")
+        println(norm(pnp_dr_state.μ - pnp_dr_state.ν)) #converges to zero
+        #println(norm(pnp_dr_state.ψ))
+        #println(norm(pnp_dr_state.ν))
+
         #println(norm(admm_state.x))
+        #println(norm(pnp_dr_state.μ))
+        #println("\n")
         #println(norm(admm_state.y))
+        #println(norm(admm_state.y - admm_state.x))
+        #println(norm(admm_state.dualres))
+        #println(f(admm_state.x) + g(admm_state.y))
     end
 
-    for pnp_dr_state in Iterators.take(pnp_dr_iter2, 100)
+    #println("\n")
+
+    #for pnp_dr_state in Iterators.take(pnp_dr_iter2, 10)
+        #println(norm(pnp_dr_state.μ)) #not quite converging, alternating between 1.04 and 1.03
+        #println(norm(pnp_dr_state.ν))
+        #println(norm(pnp_dr_state.ψ))
+        #println(norm(pnp_dr_state.x_tilde))
+        #println(norm(pnp_dr_state.y_tilde))
+
+        #println(norm(pnp_dr_state.ψ - pnp_dr_state.ν))
+
+
         #println(norm(pnp_dr_state.dualres)) #should go to zero, stalls around 1.4578023
         #println(norm(pnp_dr_state.y_tilde))
         #println(norm(pnp_dr_state.x_tilde))
-        println(norm(pnp_dr_state.μ - pnp_dr_state.ν))
-    end
+        #println(norm(pnp_dr_state.μ - pnp_dr_state.ν)) # this value tends to 0, as it should!
+        #println(norm(pnp_dr_state.ψ - pnp_dr_state.ψ_prev)) #this value also tends to 0
+
+    #end
 
 end
