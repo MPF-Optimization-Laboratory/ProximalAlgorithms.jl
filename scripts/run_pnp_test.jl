91586cc36be608d218d8e0664d984a01bda8636f
Naomi Graham <naomigra@cedar5.cedar.computecanada.ca>
2022-05-19 14:39:03 -0700

    updated to tests

--- scripts/run_pnp_test.jl
+++ scripts/run_pnp_test.jl
@@ -16,6 +16,8 @@ using Flux
 using Zygote
 
 
+# need a function to run PnP DR on a single example with various gamma, number of iterations, 
+
 function run_example_1()
     T = Float32
     R = real(T)
@@ -35,7 +37,7 @@ function run_example_1()
     test_x, test_y = MNIST.testdata(Float32,example_index)
     
     m,n = size(test_x)
-    A_full_rank = rand(Float32, (m*n, m*n))
+    A_full_rank = randn(Float32, (m*n, m*n)) #randn performs better than rand
     max_rank = Int64(floor(m*n*k))
     F = svd(A_full_rank)
 
@@ -48,17 +50,21 @@ function run_example_1()
     
     convert_and_save_single_MNIST_image(test_x_vec[:],"./scripts/temp/run_example1","ground_truth")
 
-    b = A*test_x_vec[:] # add additive noise as well?
+    #b = A*test_x_vec[:] # add additive noise as well?
+
+    # add noise to x instead of Ax
+    b = A*(test_x_vec[:] + randn(T,Int64(length(test_x_vec[:]))))
     
-    b = b + randn(T,Int64(length(b))) #adding noise
+    #b = b + randn(T,Int64(length(b))) #adding noise
     f = LeastSquares(A, b)
 
     #lam = R(0.1) * norm(A' * b, Inf)
     x0 = zeros(R, m*n)
     #x0 = test_x_vec[:]
     #gamma = R(10) / opnorm(A)^2 # constant parameter for douglas rachford
-    #gamma = 1.0f0
-    gamma = R(0.1) #trying different values of gamma
+    #gamma = R(0.1) #trying different values of gamma
+    gamma = R(1)
+    println("\n gamma is $gamma \n")
     # forward operator
     proxf!(x,z) = prox!(x,f,z,gamma)
     
@@ -66,12 +72,11 @@ function run_example_1()
 
     i = 0
     # TO DO: find a useful way to look at output
-    for pnp_dr_state in Iterators.take(pnp_dr_iter,200)
+    for pnp_dr_state in Iterators.take(pnp_dr_iter,30)
         println("iteration $i")
         println(norm(pnp_dr_state.res))
         println("\n")
         #convert_and_save_single_MNIST_image(pnp_dr_state.x,"./scripts/temp/run_example1/","iteration_$i")
-        
         resize_and_save_single_MNIST_image(pnp_dr_state.x,"./scripts/temp/run_example1/","iteration_$i")
         #println(norm(pnp_dr_state.res))
         i+=1
@@ -108,28 +113,25 @@ function run_example_2()
     # b = Ax + noise
     b = A*test_x_vec[:] + randn(T,Int64(floor(m*n*k)) ) 
 
-    # convert_and_save_single_MNIST_image(clamp.(b,0,1),"./scripts/temp/","observed")
-
     f = LeastSquares(A, b)
 
     #lam = R(0.1) * norm(A' * b, Inf)
     x0 = zeros(R, m*n)
     #x0 = test_x_vec[:]
-    #y = similar(x0)
-    gamma = R(10) / opnorm(A)^2 # constant parameter for douglas rachford using g is the l1 norm
-    #gamma = 1.0f0
+    gamma = R(10) / opnorm(A)^2 # constant parameter used douglas rachford examples for scaling the l1 norm regularizer
+    gamma = R(1)
     println("gamma is", gamma)
 
     # forward operator
-    proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma)
+    proxf!(x,u) = prox!(x,f,u,gamma)
     
-    pnp_dr_iter = PnpDrsIteration(proxf! = proxf!, denoiser! = vae_denoiser!, uhat0 = x0, gamma = gamma)
+    pnp_dr_iter = PnpDrsIteration(J_A! = proxf!, J_B! = vae_denoiser!, z0 = x0)
 
     i = 0
     # TO DO: find a useful way to look at output
     for pnp_dr_state in Iterators.take(pnp_dr_iter,10)
         #convert_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example2/","iteration_$i")
-        resize_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example2/","iteration_$i")
+        resize_and_save_single_MNIST_image(pnp_dr_state.x,"./scripts/temp/run_example2/","iteration_$i")
         println(norm(pnp_dr_state.res))
         #println(norm(test_x_vec[:] - pnp_dr_state.xhat))
         #println(norm(pnp_dr_state.xhat))
@@ -174,23 +176,18 @@ function run_example_3()
 
     #lam = R(0.1) * norm(A' * b, Inf)
     x0 = zeros(R, m*n)
-    #x0 = test_x_vec[:]
-    #y = similar(x0)
     #gamma = R(10) / opnorm(A)^2 # constant parameter for douglas rachford using g is the l1 norm
     gamma = 1.0f0
-    println("gamma is", gamma)
+    println("\n gamma is $gamma \n")
 
     # forward operator
     proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma)
     
-    pnp_dr_iter = PnpDrsIteration(proxf! = proxf!, denoiser! = identity_denoiser!, uhat0 = x0, gamma = gamma)
+    pnp_dr_iter = PnpDrsIteration(J_A! = proxf!, J_B! = identity_denoiser!, z0 = x0)
 
     i = 0
-    # TO DO: find a useful way to look at output
     for pnp_dr_state in Iterators.take(pnp_dr_iter,50)
-        #convert_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example2/","iteration_$i")
-        resize_and_save_single_MNIST_image(clamp.(pnp_dr_state.xhat,0.0,1.0),"./scripts/temp/run_example3/","iteration_$i")
-        println(norm(test_x_vec[:] - pnp_dr_state.xhat))
+        resize_and_save_single_MNIST_image(pnp_dr_state.x,"./scripts/temp/run_example3/","iteration_$i")
         i+=1
     end
 
@@ -251,22 +248,13 @@ function run_example_4()
     proxf!(x,z) = prox!(x,f,z,gamma) 
     denoiser!(y,r) = prox!(y,g,r,gamma)
 
-    #dr_iter = DouglasRachfordIteration(f=f, g=g, x0=x0, gamma=gamma)
-
     pnp_dr_iter = PnpDrsIteration(J_A! = proxf!, J_B! = denoiser!, z0 = z0)
-
-    #pnp_dr_iter2 = PnpDrsIteration2(proxf! = proxf!, denoiser! = denoiser!, x0 = x0, gamma = gamma)
 
     
     for pnp_dr_state in Iterators.take(pnp_dr_iter, 30)
         println(norm(pnp_dr_state.res))
     end
 
-    #for pnp_dr_state in Iterators.take(pnp_dr_iter2, 10)
-    #    println(norm(pnp_dr_state.dualres)) #should go to zero, stalls around 1.4578023
-    #    println(norm(pnp_dr_state.x_tilde_prev))
-        #println(norm(pnp_dr_state.μ - pnp_dr_state.ν)) # this should go to zero according to BC
-    #end
 end
 
 
@@ -291,87 +279,44 @@ function run_example_5()
         -1.0 0.0 4.0 -3.0 2.0
         -1.0 -1.0 -1.0 1.0 3.0
     ]
-    
     b = T[1.0, 2.0, 3.0, 4.0]
-
     m, n = size(A)
-
     R = real(T)
     
     #lam = R(0.1) * norm(A' * b, Inf)
     #lam = R(0.5) #what role does lambda play?
-
     lam = R(1)
 
     f = LeastSquares(A, b)
     g = NormL1(lam)
 
     fdual = NegativeConjugate(Conjugate(f))
-
     gdual = Conjugate(g)
 
     x0 = zeros(R, n)
-    u0 = zeros(R, n)
+
 
     #gamma=R(10) / opnorm(A)^2
-
-    #gamma= R(10) bigger gamma seems to make dualres bigger
-
+    #gamma= R(10) 
     gamma = R(1)
 
-    #proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma) #apply DRS to dual problem 
-    proxf!(out,in) = prox!(out,fdual,in,gamma) 
+    proxf!(x,u) = prox!(x,f,u,gamma) 
+    #proxf!(x,u) = prox!(x,fdual,u,gamma) #apply DRS to dual problem 
 
-    denoiser!(out,in) = prox!(out,g,in,gamma)
-    #denoiser!(out,in) = prox!(out,gdual,in,gamma) #apply DRS to dual problem
-
-    #proxf!(xhat,uhat) = prox!(xhat,f,uhat,gamma) #in place prox updates xhat
-    #denoiser!(yhat,rhat) = prox!(yhat,g,rhat,gamma) #rhat^{k+1} = 2xhat^{k+1} - uhat^{k}
+    denoiser!(x,u) = prox!(x,g,u,gamma)
+    #denoiser!(x,u) = prox!(x,gdual,u,gamma) #apply DRS to dual problem
 
     #dr_iter = DouglasRachfordIteration(f=f, g=g, x0=x0, gamma=gamma)
-    admm_iter = AdmmIteration(f = f, g = g, x0 = u0, gamma = gamma)
+    admm_iter = AdmmIteration(f = f, g = g, x0 = x0, gamma = gamma)
+    pnp_dr_iter = PnpDrsIteration(J_A! = proxf!, J_B! = denoiser!, z0 = x0)
     
-    pnp_dr_iter2 = PnpDrsIteration2(proxf! = proxf!, denoiser! = denoiser!, x0 = x0, gamma = gamma)
-    
-    i = 0
-
-    for (admm_state, pnp_dr_state) in Iterators.take(zip(admm_iter, pnp_dr_iter2), 1000)
-    #for admm_state in Iterators.take(admm_iter, 5)
-        #append!(dualres_vector,admm_state.dualres)
+    for (admm_state, pnp_dr_state) in Iterators.take(zip(admm_iter, pnp_dr_iter), 30)
         #println(norm(admm_state.u)) # converges to 2.072
-        #println(norm(admm_state.x - admm_state.y))
-        #println("\n")
-        println(norm(pnp_dr_state.μ - pnp_dr_state.ν)) #converges to zero
-        #println(norm(pnp_dr_state.ψ))
-        #println(norm(pnp_dr_state.ν))
-
-        #println(norm(admm_state.x))
-        #println(norm(pnp_dr_state.μ))
-        #println("\n")
-        #println(norm(admm_state.y))
-        #println(norm(admm_state.y - admm_state.x))
-        #println(norm(admm_state.dualres))
-        #println(f(admm_state.x) + g(admm_state.y))
+        println(norm(admm_state.x - admm_state.y)) #converges to zero
+        println(norm(pnp_dr_state.res)) #converges to zero
+        println("\n")
     end
 
-    #println("\n")
 
-    #for pnp_dr_state in Iterators.take(pnp_dr_iter2, 10)
-        #println(norm(pnp_dr_state.μ)) #not quite converging, alternating between 1.04 and 1.03
-        #println(norm(pnp_dr_state.ν))
-        #println(norm(pnp_dr_state.ψ))
-        #println(norm(pnp_dr_state.x_tilde))
-        #println(norm(pnp_dr_state.y_tilde))
-
-        #println(norm(pnp_dr_state.ψ - pnp_dr_state.ν))
-
-
-        #println(norm(pnp_dr_state.dualres)) #should go to zero, stalls around 1.4578023
-        #println(norm(pnp_dr_state.y_tilde))
-        #println(norm(pnp_dr_state.x_tilde))
-        #println(norm(pnp_dr_state.μ - pnp_dr_state.ν)) # this value tends to 0, as it should!
-        #println(norm(pnp_dr_state.ψ - pnp_dr_state.ψ_prev)) #this value also tends to 0
-
-    #end
 
 end
