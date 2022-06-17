c4be4c10f1dac864ea9ec5482e50177b626dc1e5
Naomi Graham <naomigra@cedar1.cedar.computecanada.ca>
2022-06-17 14:11:09 -0700

    complete script that does classification and denoising

--- scripts/run_classify_and_denoise.jl
+++ scripts/run_classify_and_denoise.jl
@@ -2,6 +2,9 @@
 ## this code is copied from 
 ## https://github.com/ZhenanFanUBC/classified-compressed-sensing/blob/master/code/compressed_sensing_mnist_classifier.jl
 #############################
+include("../src/utilities/denoisers.jl")
+include("../src/utilities/image_tools.jl")
+
 
 using Flux
 using MLDatasets
@@ -33,6 +36,8 @@ trainY = onehotbatch(y_train, 0:9)
 loss(x, y) = crossentropy(model(x), y)
 # ground truth
 xs = trainX[:, 2]; ys = trainY[:, 2]
+filename = "ground_truth"
+save(joinpath(".", "$filename.png"), MNIST.convert2image(xs))
 # measurement
 m = 392; M = randn(m, 784); b = M*xs
 # objective
@@ -44,15 +49,26 @@ lower = zeros(784); upper = ones(784)
 # initial point
 x0 = ones(784)/2
 # solve the optimization problem
-@printf("solve optimization problem ... ")
+@printf("solving optimization problem ... ")
 result = optimize(f, g, lower, upper, x0; inplace = false)
 @printf("done\n")
 
 # show relative difference
 x = Optim.minimizer(result)
 @printf("relative difference = %.2f ", norm(x - xs)/norm(xs))
-
+x = 1 .- x
 # plot
 #imshow(reshape(x, 28, 28))
-filename = "classifier_result"
+filename = "classifier_result_before_denoise"
+save(joinpath(".", "$filename.png"), MNIST.convert2image(x))
+
+model_epoch_number = 20
+model_dir = "./src/utilities/saved_models/MNIST"
+encoder_μ, encoder_logvar, decoder = load_model(model_dir,model_epoch_number)
+
+@printf("applying denoiser ... ")
+
+x .= reconstruct_images(encoder_μ, encoder_logvar, decoder,x)
+
+filename = "classifier_result_after_denoise"
 save(joinpath(".", "$filename.png"), MNIST.convert2image(x))
